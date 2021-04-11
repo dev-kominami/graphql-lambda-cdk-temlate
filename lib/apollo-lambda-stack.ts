bd7e2d9dc941ac084077997532abba5c5165b1f7
Takashi Kominami <dev.kominami@gmail.com>
2021-04-11 20:32:10 +0900

    feat(lambda): hello Lambda作成

--- lib/apollo-lambda-stack.ts
+++ lib/apollo-lambda-stack.ts
@@ -1,9 +1,14 @@
 import * as cdk from '@aws-cdk/core';
-
+import * as lambda from '@aws-cdk/aws-lambda'
 export class ApolloLambdaStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    new lambda.Function(this, 'apollo-hello', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'apollo/hello.handler',
+      code: lambda.Code.fromAsset('lambda')
+    })  
   }
 }
