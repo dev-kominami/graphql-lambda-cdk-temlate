fce1c74a18f0e5a307273e4cfc6de94204111fce
Takashi Kominami <dev.kominami@gmail.com>
2021-04-11 21:50:34 +0900

    feat(apigateway): apigateway 設定

--- lib/apollo-lambda-stack.ts
+++ lib/apollo-lambda-stack.ts
@@ -1,14 +1,20 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda'
+import * as apigw from '@aws-cdk/aws-apigateway'
 export class ApolloLambdaStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
-    new lambda.Function(this, 'apollo-hello', {
+    const hello = new lambda.Function(this, 'apollo-hello', {
       runtime: lambda.Runtime.NODEJS_14_X,
       handler: 'apollo/hello.handler',
-      code: lambda.Code.fromAsset('lambda')
+      code: lambda.Code.fromAsset('lambda'),
+      description: 'GraphQL Apollo Server'
     })  
+
+    new apigw.LambdaRestApi(this, 'apollo-graphql-api', {
+      handler: hello,
+    })
   }
 }
